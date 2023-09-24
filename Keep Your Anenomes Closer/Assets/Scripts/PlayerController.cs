8a79af3f5ca8439c1f0ab899ddea976940937b9f
N3onNarwhal <averytlanier@icloud.com>
2023-09-24 15:57:27 -0500

    eeee

---
+++ Keep Your Anenomes Closer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float slowDiagonal = 0.7f;
+
+    public float moveSpeed = 0.5f;
+    bool facingLeft = true;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // check for diagonal movement and slow it down
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= slowDiagonal;
+            vertical *= slowDiagonal;
+        }
+
+        // keep player in-bounds on y-axis
+        if (transform.position.y >= 36.3f)
+        {
+            transform.position = new Vector3(transform.position.x, 36.5f, 0);
+        }
+        else if (transform.position.y <= -36.3f)
+        {
+            transform.position = new Vector3(transform.position.x, -36.5f, 0);
+        }
+
+        // keep player in-bounds on x-axis
+        if (transform.position.x >= 124.4f)
+        {
+            transform.position = new Vector3(124.6f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -124.4f)
+        {
+            transform.position = new Vector3(-124.6f, transform.position.y, 0);
+        }
+
+        body.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+
+        if (horizontal > 0 && facingLeft)
+        {
+            Flip();
+        }
+        else if (horizontal < 0 && !facingLeft)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingLeft = !facingLeft;
+    }
+}
