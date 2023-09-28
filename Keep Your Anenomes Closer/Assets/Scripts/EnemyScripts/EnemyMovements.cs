befc7a1f2f32e636ced202529483814a9258052e
N3onNarwhal <averytlanier@icloud.com>
2023-09-27 20:26:51 -0500

    Flipperoo

--- Keep Your Anenomes Closer/Assets/Scripts/EnemyScripts/EnemyMovements.cs
+++ Keep Your Anenomes Closer/Assets/Scripts/EnemyScripts/EnemyMovements.cs
@@ -6,14 +6,17 @@ public class EnemyMovements : MonoBehaviour
 {
     public Transform player;
     public float moveSpeed = 2f;
+    // private bool spriteLeft = true;
+    private PlayerController c;
     private Rigidbody2D rb;
     private Vector2 movement; // equal to target direction
     private PlayerAwarenessControl _playerAwarenessControl;
-
+    private GameObject g;
 
     // Start is called before the first frame update
     private void Awake()
     {
+        g = GameObject.Find("Player");
         rb = this.GetComponent<Rigidbody2D>();
         _playerAwarenessControl = GetComponent<PlayerAwarenessControl>();
     }
@@ -26,9 +29,9 @@ public class EnemyMovements : MonoBehaviour
             //Track player
             Vector3 direction = player.position - transform.position;
 
-            //Tracks rotation 
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;
+            // //Tracks rotation 
+            // float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            // rb.rotation = angle;
 
             //Movement variables
             direction.Normalize();
@@ -36,12 +39,32 @@ public class EnemyMovements : MonoBehaviour
 
         }
     }
+
     private void FixedUpdate()
     {
+        c = g.GetComponent<PlayerController>();
+        if (c.horizontal != 0) {
+            if (c.horizontal > 0 && c.facingLeft)
+            {
+                Flip();
+            }
+            else if (c.horizontal < 0 && !c.facingLeft)
+            {
+                Flip();
+            }
+        }
         moveCharater(movement);
     }
+
     void moveCharater(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
     }
 }
