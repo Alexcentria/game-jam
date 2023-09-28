f48eacb0f1c4477be75725b5ffbc976cc3875313
kaptkyle <145627044+Laursaurus@users.noreply.github.com>
2023-09-27 19:54:55 -0500

    heheh

--- Keep Your Anenomes Closer/Assets/Scripts/EnemyScripts/EnemyMovements.cs
+++ Keep Your Anenomes Closer/Assets/Scripts/EnemyScripts/EnemyMovements.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class EnemyMovements : MonoBehaviour
 {
+    public Transform player;
+    public float moveSpeed = 2f;
+    private Rigidbody2D rb;
+    private Vector2 movement; // equal to target direction
+    private PlayerAwarenessControl _playerAwarenessControl;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+        _playerAwarenessControl = GetComponent<PlayerAwarenessControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_playerAwarenessControl.AwareOfPlayer)
+        {
+            //Track player
+            Vector3 direction = player.position - transform.position;
+
+            //Tracks rotation 
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+
+            //Movement variables
+            direction.Normalize();
+            movement = direction;
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        moveCharater(movement);
+    }
+    void moveCharater(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
 }
