00ed9dcb23e4e9ae511ccb05b5d06dc2cf04eb70
N3onNarwhal <averytlanier@icloud.com>
2023-09-30 19:32:11 -0500

    Collision sound added

--- Keep Your Anenomes Closer/Assets/Scripts/KillCollision.cs
+++ Keep Your Anenomes Closer/Assets/Scripts/KillCollision.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class KillCollision : MonoBehaviour
 {
+
+    public AudioSource audioPlayer;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("Collided");
             Destroy(collision.gameObject);
+            audioPlayer.Play();
         }
-    }
+    } 
 }
